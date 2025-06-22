0aff86d97aa26ed98593bf47b0ad654dccdb7a61
Hidenori Ohnishi <8862042+hidenoriohnishi@users.noreply.github.com>
2025-06-22 15:34:37 +0900

    Organized AI model

--- src/renderer/components/settings/PromptDetail.tsx
+++ src/renderer/components/settings/PromptDetail.tsx
@@ -10,8 +10,9 @@ const aiproviders = [
 const openAIModels = [
   'gpt-4o',
   'gpt-4o-mini',
-  'gpt-4-turbo',
-  'gpt-3.5-turbo',
+  'gpt-4.1',
+  'gpt-4.1-mini',
+  'gpt-4.1-nano'
 ]
 const anthropicModels = [
   'claude-3-opus-20240229',
